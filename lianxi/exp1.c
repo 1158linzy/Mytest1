f18ebb20db7c878437a0c0f19e200c41a5625303
1158linzy <linzy@tech-now.com>
2022-07-12 14:40:26 +0800

    just a test

---
+++ lianxi/exp1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "test.h"
+int add(int a,int b)
+{
+  return a+b;
+}
+int main()
+{
+   
+   printf("2+3=%d\n",add(2,3));
+   printf("5-4=%d\n",sub(5,4));
+   return 0;
+}
